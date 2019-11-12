375caa44b6cf39c78b3b1011d94713021ec935a1
Jason <ironelder@gmail.com>
2019-11-12 21:26:56 +0900

    ServerAPI 명세 작성
    ServerAPI 명세 작성

--- app/src/main/java/com/ironelder/kotlintodolist/data/ITodoRemoteApi.kt
+++ app/src/main/java/com/ironelder/kotlintodolist/data/ITodoRemoteApi.kt
@@ -1,4 +1,30 @@
 package com.ironelder.kotlintodolist.data
 
+import retrofit2.Call
+import retrofit2.http.*
+
 interface ITodoRemoteApi {
+
+    @GET("todos")
+    fun requestGetAllTodoList(): Call<ArrayList<TodoModel>>
+
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @FormUrlEncoded
+    @POST("todos")
+    fun requestCreateTodoItem(@Field("content") data:String): Call<TodoModel>
+
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @FormUrlEncoded
+    @PUT("todos/{itemId}")
+    fun requestUpdateContentTodoItem(@Path("itemId") itemId:Int, @Field("content") data:String): Call<TodoModel>
+
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @DELETE("todos/{itemId}")
+    fun requestDeleteTodoItem(@Path("itemId") itemId:Int): Call<ArrayList<TodoModel>>
+
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @FormUrlEncoded
+    @PUT("todos/{itemId}")
+    fun requestUpdateStatusTodoItem(@Path("itemId") itemId:Int, @Field("done") data:Boolean): Call<TodoModel>
+
 }
