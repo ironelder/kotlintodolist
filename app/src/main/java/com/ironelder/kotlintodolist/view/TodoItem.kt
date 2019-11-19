fbd18955e600ac47a9a12000a970920c37f3d6a3
Jason <ironelder@gmail.com>
2019-11-19 20:51:58 +0900

    recyclerView Adapter 구체화
    recyclerView Adapter 구체화

--- app/src/main/java/com/ironelder/kotlintodolist/view/TodoItem.kt
+++ app/src/main/java/com/ironelder/kotlintodolist/view/TodoItem.kt
@@ -8,7 +8,7 @@ import com.ironelder.kotlintodolist.R
 import com.ironelder.kotlintodolist.data.TodoModel
 import kotlinx.android.synthetic.main.layout_todo_item.view.*
 
-class TodoItem(context:Context?) : ConstraintLayout(context) {
+class TodoItem(context: Context?) : ConstraintLayout(context) {
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_todo_item, this, true)
         layoutParams = RecyclerView.LayoutParams(
@@ -16,8 +16,9 @@ class TodoItem(context:Context?) : ConstraintLayout(context) {
             RecyclerView.LayoutParams.WRAP_CONTENT
         )
     }
-    fun setData(todoItem:TodoModel){
-        et_todo_content.setText(todoItem.content)
-        tv_todo_date.text = todoItem.updatedAt?:todoItem.createdAt
+
+    fun setData(todoData: TodoModel) {
+        et_todo_content.setText(todoData.content)
+        tv_todo_date.text = todoData.updatedAt ?: todoData.createdAt
     }
 }
--- app/src/main/java/com/ironelder/kotlintodolist/view/TodoListAdapter.kt
+++ app/src/main/java/com/ironelder/kotlintodolist/view/TodoListAdapter.kt
@@ -1,4 +1,54 @@
 package com.ironelder.kotlintodolist.view
 
-class TodoListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ironelder.kotlintodolist.data.TodoModel
+
+class TodoListAdapter : RecyclerView.Adapter<TodoListAdapter.TodoItemHolder>() {
+    private var mTodoItemList = arrayListOf<TodoModel>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoItemHolder {
+        val todoItemView = TodoItem(parent.context)
+        return TodoItemHolder(todoItemView)
+    }
+
+    override fun getItemCount() = mTodoItemList.size
+
+    override fun onBindViewHolder(holder: TodoItemHolder, position: Int) {
+        holder.setData(mTodoItemList[position])
+    }
+
+    fun setTodoItemList(todoItemList: ArrayList<TodoModel>) {
+        mTodoItemList.clear()
+        if (!todoItemList.isNullOrEmpty()) {
+            mTodoItemList.addAll(todoItemList)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun clearTodoItemList() {
+        mTodoItemList.clear()
+        notifyDataSetChanged()
+    }
+
+    fun addTodoItem(todoItem: TodoModel) {
+        mTodoItemList.add(todoItem)
+        notifyItemInserted(mTodoItemList.size - 1)
+    }
+
+    fun addTodoItemList(todoItemList: ArrayList<TodoModel>?) {
+        mTodoItemList.addAll(todoItemList ?: arrayListOf())
+        notifyItemRangeInserted(mTodoItemList.size - 1, todoItemList?.size ?: 0)
+    }
+
+    inner class TodoItemHolder(view: TodoItem) : RecyclerView.ViewHolder(view) {
+        private val todoItemView: TodoItem by lazy {
+            view
+        }
+
+        fun setData(todoData: TodoModel) {
+            todoItemView.setData(todoData)
+        }
+    }
+
+
 }
